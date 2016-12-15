ffedd9176ab6e04d07eff4800e4b93241904de4b
Le Tien Tai <letientai299@gmail.com>
2016-12-15 18:32:09 +0700

    Init project with lots of todos

---
+++ auto-toc.user.js
@@ -0,0 +1,139 @@
+// ==UserScript==
+// @name    Auto TOC
+// @namespace   mine
+// @include   https://*/*
+// @include   http://*/*
+// @include   file://*
+// @version   1
+// @require   https://code.jquery.com/jquery-3.1.1.min.js
+// @require   https://raw.githubusercontent.com/jgallen23/toc/master/dist/toc.min.js
+// @grant     GM_addStyle
+// ==/UserScript==
+
+/**
+ * ID of the TOC element.
+ */
+let tocId = "minetoc"
+
+let includedSitePatterns = [
+  /.*.wiki.*\/.*/,
+  /.*medium.com\/.*/,
+  /.*.github.com\/.*/,
+  /file:\/\/\/.*/
+];
+
+/**
+ * Test whehter the website location is excluded by default (TOC won't displaying).
+ */
+let isOnIncludedSites = function (location) {
+  return includedSitePatterns.some((pattern) => {
+    return pattern.test(location)
+  })
+};
+
+/**
+ * Keep track of TOC displaying status
+ */
+var isTocDisplaying = true;
+
+let setTocDisplayMode = function (isDisplay) {
+  let cssDisplay = isDisplay ? "block" : "none";
+  $(`#${tocId}`).css("display", cssDisplay);
+}
+
+let toggleToc = function () {
+  isTocDisplaying = !isTocDisplaying;
+  setTocDisplayMode(isTocDisplaying);
+}
+
+/**
+ * Build the TOC using jquery and toc plugin.
+ */
+$("body").append(`<div id=\"${tocId}\"></div>`);
+$(`#${tocId}`).toc({
+  'selectors': 'h1,h2,h3,h4,h5', //elements to use as headings
+});
+
+if (isOnIncludedSites(window.location)) {
+  isTocDisplaying = true;
+  setTocDisplayMode(isTocDisplaying);
+}
+
+/**
+ * Add shortcut to toggle the TOC
+ */
+document.addEventListener('keydown', function (e) {
+
+  // Alt+q
+  if (e.keyCode == 81 && !e.shiftKey && !e.ctrlKey && e.altKey && !e.metaKey) {
+    toggleToc();
+  }
+}, false);
+
+
+GM_addStyle(`
+#${tocId} {
+  display: none;
+  float: left;
+  overflow: auto;
+  resize: horizontal;
+  top: 0px;
+  left: 0px;
+  height: 100%;
+  position: fixed;
+  background: #222;
+  box-shadow: inset -5px 0 5px 0px #000;
+  width: 200px;
+  max-width: 200px;
+  padding-top: 80px;
+  color: #fff !important;
+  z-index: 99999;
+}
+
+#${tocId} ul {
+  margin: 0;
+  padding: 0;
+}
+
+#${tocId} li {
+  padding: 5px 10px;
+}
+
+#${tocId} a {
+  color: #fff;
+  text-decoration: none;
+  display: block;
+}
+
+#${tocId} .toc-h1 {
+  font-size: 1.5rem;
+}
+
+#${tocId} .toc-h2 {
+  padding-left: 10px;
+  font-size: 1.3rem;
+  opacity: 0.9;
+}
+
+#${tocId} .toc-h3 {
+  padding-left: 20px;
+  font-size: 1.1rem;
+  opacity: 0.8;
+}
+
+#${tocId} .toc-h4 {
+  padding-left: 30px;
+  font-size: 0.9rem;
+  opacity: 0.7;
+}
+
+#${tocId} .toc-h5 {
+  padding-left: 40px;
+  opacity: 0.6;
+}
+
+#${tocId} .toc-active {
+  background: #336699;
+  box-shadow: inset -5px 0px 10px -5px #000;
+}
+`);
---
+++ readme.md
@@ -0,0 +1,65 @@
+TODO
+=====
+
+
+Doc
+---
+
+- [ ] Write proper Readme.
+- [ ] Document source code.
+
+
+Marketing
+---------
+
+- [ ] Make a github.io page for this.
+- [ ] Write a post about how to make the basis of this plugins
+- [ ] Upload on OpenUserJS, Userscript...
+- [ ] Post to Hacker News.
+- [ ] Post to Twitter
+- [ ] Clean up junk in my Github Profiles
+- [ ] Share to friends for testing
+
+
+Enhancements
+------------
+
+- [ ] Should be only enable on some sites. Should have shortcut to add site.
+- [ ] Icons, animations.
+- [ ] Better Article detections
+- [ ] Custom shortcut key
+- [ ] Expand, Close
+- [ ] More shortcut keys: Zooms, Search, Next, Back, Expand, Close
+- [ ] Custom css
+- [ ] More default style
+- [ ] Settings page
+- [ ] Sync settings with Firefox/Google profiles
+- [ ] Make it an standalone addon, not depend on Greasemonkey.
+- [ ] Test on other user script engines.
+- [ ] Test on other browser.
+- [ ] Resizable
+- [ ] Movable (floating mode)
+- [ ] Left/Right
+- [ ] TOC search
+- [ ] Integrate with VimFx mark and `g[`, `g]` features
+
+
+Bugs
+----
+
+- [ ] TOC show on all `body` tags, include one under `iframe`
+- [ ] TOC show also the hidden elements, when use with Stylish custom style.
+- [ ] Auto highlighted header is not works correctly.
+- [ ] CSS is override by other plugins/scripts.
+- [ ] Shortcut doesn't work if the page is not focused.
+
+
+Dev
+---
+
+- [ ] Auto test
+- [ ] Auto reload
+- [ ] npm setup
+- [ ] Separate js, css, html.
+
+
